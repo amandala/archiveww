f2a3215184c18781eed37f79287ae916ebbcafdd
amandala <{ID}+{username}@users.noreply.github.com>
2023-09-15 13:54:42 -0600

    FourOhFour page OG updates

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,29 +1,13 @@
 import PageHead from "@/components/PageHead";
 import UnderConstruction from "@/components/UnderConstruction";
 
-// export default function FourOhFour() {
-//   return (
-//     <>
-//       <div className={styles.Wrapper} style={{backgroundImage: `url(${background.src})`}}>
-//         <h1 className={cx(chunkyHeart.className, styles.Number)}>404</h1>
-//         <h2 className={cx(dinCondensed.className, styles.Heading)}>
-//           Nothing to see here!
-//         </h2>
-//         <div className={styles.ButtonWrapper}>
-//           <Button href="/">Go back home</Button>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
 export default function FourOhFour() {
   return (
     <>
       <PageHead
-        page="404"
+        page="404 - Page Not Found"
         image="https://drive.google.com/uc?export=view&id=1iLKmD6dn_beuWGj9DSJ2sJ0HeD4p0AIC&v=3"
-        description="Page not found."
+        description="Page not found. Visit wickedwoods.ca"
         url="wickedwoods.ca"
       />
       <UnderConstruction />
