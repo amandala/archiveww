0de25ff926d0c278e5a5c44aa977369631dc0270
amandala <{ID}+{username}@users.noreply.github.com>
2023-09-16 21:41:39 -0600

    spice up under constucton

--- src/components/UnderConstruction/index.tsx
+++ src/components/UnderConstruction/index.tsx
@@ -5,6 +5,9 @@ import greenHazeBackground from "/public/assets/backgrounds/green_haze-min.png";
 import { JumboHeading, Paragraph } from "../Typography";
 
 import beaver from "/public/assets/Beavz3.png";
+import tree from "/public/assets/Tree_1.png";
+import tree2 from "/public/assets/Tree_2.png";
+import tree3 from "/public/assets/Tree_3.png";
 
 export default function UnderConstruction({
   title = "Under Construction",
@@ -39,6 +42,104 @@ export default function UnderConstruction({
               margin: "0 auto",
               width: "100%",
               height: "auto",
+            }}
+          />
+        </div>
+        <div className={styles.TreeWrapper}>
+          <Image
+            alt="A nice tree"
+            src={tree}
+            priority
+            style={{
+              objectFit: "contain",
+
+              maxWidth: 800,
+              margin: "0 auto",
+              width: "100%",
+              height: "auto",
+              position: "absolute",
+              bottom: 0,
+              left: "-10%",
+            }}
+          />
+          <Image
+            alt="A nice tree"
+            src={tree}
+            priority
+            style={{
+              objectFit: "contain",
+
+              maxWidth: 600,
+              margin: "0 auto",
+              width: "100%",
+              height: "auto",
+              position: "absolute",
+              bottom: 0,
+              left: "10%",
+            }}
+          />
+          <Image
+            alt="A nice tree"
+            src={tree2}
+            priority
+            style={{
+              objectFit: "contain",
+
+              maxWidth: 700,
+              margin: "0 auto",
+              width: "100%",
+              height: "auto",
+              position: "absolute",
+              bottom: 0,
+              left: "-20%",
+            }}
+          />
+          <Image
+            alt="A nice tree"
+            src={tree2}
+            priority
+            style={{
+              objectFit: "contain",
+
+              maxWidth: 700,
+              margin: "0 auto",
+              width: "100%",
+              height: "auto",
+              position: "absolute",
+              bottom: 0,
+              right: "-20%",
+            }}
+          />
+          <Image
+            alt="A nice tree"
+            src={tree3}
+            priority
+            style={{
+              objectFit: "contain",
+
+              maxWidth: 800,
+              margin: "0 auto",
+              width: "100%",
+              height: "auto",
+              position: "absolute",
+              bottom: 0,
+              right: "-10%",
+            }}
+          />
+          <Image
+            alt="A nice tree"
+            src={tree}
+            priority
+            style={{
+              objectFit: "contain",
+
+              maxWidth: 500,
+              margin: "0 auto",
+              width: "100%",
+              height: "auto",
+              position: "absolute",
+              bottom: 0,
+              right: "10%",
             }}
           />
         </div>
