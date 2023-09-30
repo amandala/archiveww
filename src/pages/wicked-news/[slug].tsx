e973985f7ff5f40afb2e0bba4cce45747319b0a9
amandala <{ID}+{username}@users.noreply.github.com>
2023-09-30 16:39:21 -0600

    Wicked News blog up V1

---
+++ src/pages/wicked-news/[slug].tsx
@@ -0,0 +1,44 @@
+import { JumboHeading, Paragraph, H2 } from "@/components/Typography";
+import { useRouter } from "next/router";
+
+import { wickedNewsData } from "@/data/wicked-news";
+
+import styles from "./article.module.scss";
+import { useState } from "react";
+
+import cx from "classnames";
+
+export default function WickedNewsArticle() {
+  const router = useRouter();
+  const [nightMode, setNightMode] = useState<boolean>(true);
+
+  const article = wickedNewsData.find(
+    (item) => item.slug === router.query.slug
+  );
+
+  return (
+    <div className={cx(styles.Page, { [styles.PageNight]: nightMode })}>
+      <div className={styles.TitleWrapper}>
+        <span>
+          <button
+            className={styles.nightToggle}
+            onClick={() => setNightMode(!nightMode)}
+          >
+            <div
+              id="moon"
+              className={cx(nightMode ? styles.moon : styles.sun)}
+            ></div>
+          </button>
+        </span>
+        <JumboHeading dark={!nightMode}>{article?.title}</JumboHeading>
+        <H2 dark={!nightMode}>{article?.subtitle}</H2>
+        <Paragraph dark={!nightMode}>{article?.published}</Paragraph>
+      </div>
+      {article?.text.map((paragraph) => (
+        <Paragraph dark={!nightMode} key={paragraph.slice(3, 10)}>
+          {paragraph}
+        </Paragraph>
+      ))}
+    </div>
+  );
+}
