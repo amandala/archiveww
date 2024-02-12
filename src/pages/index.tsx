1939f1cbaf30082cb76311436923bbb4b3b72059
amandala <6064242+amandala@users.noreply.github.com>
2024-02-12 14:55:20 -0700

    Stages page, add new line to Apply page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <PageHead
         page="Home"
         image="https://drive.google.com/uc?export=view&id=1iLKmD6dn_beuWGj9DSJ2sJ0HeD4p0AIC&v=3"
-        description="A three day arts and music festival located in the Colombia Valley just outside Fairmont BC, Canada."
+        description="A three day arts and music festival located in the Columbia Valley just outside Fairmont BC, Canada."
         url="wickedwoods.ca"
       />
       <main>
