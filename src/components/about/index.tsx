912a750a04bb00f6087aa2e3e17f77cb3e2bd551
amandala <6064242+amandala@users.noreply.github.com>
2024-02-19 14:49:59 -0700

    Update home page

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Image from "next/image";
 
+import cx from "classnames";
+
 import Gallery from "@/components/Gallery";
 
-import wordmark from "/public/assets/FONT_WITH_MF_2024.png";
-import datestamp from "/public/assets/DATE_STAMP2024.png";
+import logo from "/public/assets/2024logo.png";
 import heroImage from "/public/assets/skyvalley.jpg";
-import { JumboHeading, Paragraph } from "@/components/Typography";
+import { H3, JumboHeading, Paragraph } from "@/components/Typography";
 import YouTubePlayer from "@/components/YoutubePlayer";
 import PageHead from "@/components/PageHead";
 
@@ -35,73 +36,87 @@ export default function AboutPage() {
           className={styles.Hero}
           style={{ backgroundImage: `url(${heroImage.src})` }}
         >
-          <Image
-            alt="Wicked Woods Music Festival Wordmark Logo"
-            src={wordmark}
-            className={styles.Wordmark}
-            fill
-          />
-          <Image
-            alt="Wicked Woods Music Festival Wordmark Logo"
-            src={datestamp}
-            className={styles.DateStamp}
-            fill
-          />
+          <div className={styles.LogoWrapper}>
+            <Image
+              alt="Wicked Woods Music Festival Logo"
+              src={logo}
+              className={styles.Wordmark}
+            />
+          </div>
+          <H3 className={styles.Date}>August 30 - September 1</H3>
           <div className={styles.Button}>
             <TicketButton />
           </div>
         </div>
+        <div className={styles.PageContent}>
+          <div className={styles.DescriptionWrapper}>
+            <div className={styles.Description}>
+              <div className={styles.TextWrapper}>
+                <Paragraph className={styles.DescriptionText}>
+                  Held on traditional unceded territory of the Akisqnuk First
+                  Nation, Wicked Woods Music and Arts Festival is is an annual
+                  celebration of life through sight & sound.
+                </Paragraph>
+                <Paragraph className={styles.DescriptionText}>
+                  Nestled on the edge of the Canadian Rockies, our intimate
+                  festival is just minutes from Fairmont Hot Springs BC
+                  overlooking the Columbia Valley.
+                </Paragraph>
+                <Paragraph className={styles.DescriptionText}>
+                  We strongly value community, expression, and connection both
+                  with our fellow humans and the land on which we gather.
+                </Paragraph>
+              </div>
+            </div>
+            <div className={styles.VideoWrapper}>
+              <YouTubePlayer videoId="Ofp6omfL4r4" opts={{ loop: 1 }} />
+            </div>
 
-        <div className={styles.Gallery}>
-          <Gallery
-            images={generateGalleryImages(galleryA)}
-            targetRowHeight={300}
-          />
-        </div>
-        <div className={styles.DescriptionWrapper}>
-          <div className={styles.Description}>
-            <div className={styles.TextWrapper}>
-              <Paragraph className={styles.DescriptionText}>
-                Held on traditional unceded territory of the Akisqnuk First
-                Nation, Wicked Woods Music and Arts Festival is is an annual
-                celebration of life through sight & sound.
-              </Paragraph>
-              <Paragraph className={styles.DescriptionText}>
-                Nestled on the edge of the Canadian Rockies, our intimate
-                festival is just minutes from Fairmont Hot Springs BC
-                overlooking the Columbia Valley.
-              </Paragraph>
-              <Paragraph className={styles.DescriptionText}>
-                We strongly value community, expression, and connection both
-                with our fellow humans and the land on which we gather.
-              </Paragraph>
+            <div className={cx(styles.Gallery, styles.HideMobile)}>
+              <Gallery
+                images={generateGalleryImages(galleryB)}
+                targetRowHeight={200}
+              />
             </div>
           </div>
-          <div className={styles.VideoWrapper}>
-            <YouTubePlayer videoId="Ofp6omfL4r4" opts={{ loop: 1 }} />
-          </div>
+          <div className={styles.Applications}>
+            <div className={styles.Application}>
+              <div>
+                <JumboHeading>Calling all Creatives!!</JumboHeading>
+                <Paragraph>Our Creative Applications are NOW OPEN!</Paragraph>
+              </div>
 
+              <Link href="/apply">
+                <div
+                  style={{ backgroundImage: `url(${sky.src})` }}
+                  className={styles.ApplyButton}
+                >
+                  <JumboHeading className={styles.ApButt}>APPLY</JumboHeading>
+                </div>
+              </Link>
+            </div>
+            <div className={styles.Application}>
+              <div>
+                <JumboHeading>Join the Crew!</JumboHeading>
+                <Paragraph>Our Crew Applications open Feb.20th!</Paragraph>
+              </div>
+
+              <Link href="/apply">
+                <div
+                  style={{ backgroundImage: `url(${sky.src})` }}
+                  className={styles.ApplyButton}
+                >
+                  <JumboHeading className={styles.ApButt}>APPLY</JumboHeading>
+                </div>
+              </Link>
+            </div>
+          </div>
           <div className={styles.Gallery}>
             <Gallery
-              images={generateGalleryImages(galleryB)}
-              targetRowHeight={200}
+              images={generateGalleryImages(galleryA)}
+              targetRowHeight={300}
             />
           </div>
-        </div>
-        <div className={styles.Applications}>
-          <div>
-            <JumboHeading>Calling all Creatives!!</JumboHeading>
-            <Paragraph>Our Creative Applications are NOW OPEN!</Paragraph>
-          </div>
-
-          <Link href="/apply">
-            <div
-              style={{ backgroundImage: `url(${sky.src})` }}
-              className={styles.ApplyButton}
-            >
-              <JumboHeading className={styles.ApButt}>APPLY</JumboHeading>
-            </div>
-          </Link>
         </div>
       </div>
     </div>
